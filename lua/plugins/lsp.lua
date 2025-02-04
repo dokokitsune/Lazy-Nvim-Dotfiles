f620589f268efb8ffec2f2bc0154356001443b5d
Weston Wood <stakin101@gmail.com>
2025-02-03 19:10:36 -0800

    Added dashboard

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,47 +1,46 @@
 return {
 
-	{
-		"williamboman/mason.nvim",
+  {
+    "williamboman/mason.nvim",
 
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
+    config = function()
+      require("mason").setup()
+    end,
+  },
+  {
 
-		"williamboman/mason-lspconfig.nvim",
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = {
-					"lua_ls",
-					"bashls",
-					"html",
-					"htmx",
-					"ts_ls",
-					"pyright",
-					"pylsp",
-					"nginx_language_server",
-					"clangd",
-					"vimls",
-					"dockerls",
-				},
-			})
-		end,
-	},
-	{
+    "williamboman/mason-lspconfig.nvim",
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "lua_ls",
+          "bashls",
+          "html",
+          "htmx",
+          "ts_ls",
+          "pyright",
+          "pylsp",
+          "nginx_language_server",
+          "clangd",
+          "vimls",
+          "dockerls",
+        },
+      })
+    end,
+  },
+  {
 
-		"neovim/nvim-lspconfig",
+    "neovim/nvim-lspconfig",
 
-		config = function()
-			local lspconfig = require("lspconfig")
-			local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
-			lspconfig.lua_ls.setup({})
-			lspconfig.clangd.setup({})
-			lspconfig.ts_ls.setup({})
-			lspconfig.pyright.setup({})
-			lspconfig.pylsp.setup({})
-			lspconfig.vimls.setup({})
-		end,
-	},
+    config = function()
+      local lspconfig = require("lspconfig")
+      local lsp_capabilities = require("cmp_nvim_lsp").default_capabilities()
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      lspconfig.lua_ls.setup({})
+      lspconfig.clangd.setup({})
+      lspconfig.pyright.setup({})
+      lspconfig.pylsp.setup({})
+      lspconfig.vimls.setup({})
+    end,
+  },
 }
