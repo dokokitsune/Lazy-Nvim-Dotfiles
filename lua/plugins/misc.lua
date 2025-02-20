80e85132d6de6629f1daa4cd84f8dc933a8a13b5
Weston Wood <stakin101@gmail.com>
2025-02-19 23:47:46 -0800

    Added terraform lsp and linting

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -42,12 +42,4 @@ return {
 			},
 		},
 	},
-	{
-		"edluffy/hologram.nvim",
-		config = function()
-			require("hologram").setup({
-				auto_display = true, -- WIP automatic markdown image display, may be prone to breaking
-			})
-		end,
-	},
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,34 +1,35 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate",
-		config = function()
-			local config = require("nvim-treesitter.configs")
-			config.setup({
-				ensure_installed = {
-					"lua",
-					"javascript",
-					"cpp",
-					"css",
-					"html",
-					"nix",
-					"scss",
-					"typescript",
-					"yaml",
-					"json",
-					"toml",
-					"python",
-					"fish",
-					"bash",
-					"c",
-					"dockerfile",
-					"tsx",
-					"gitignore",
-				},
-				highlight = true,
-				sync_install = false,
-				indent = true,
-			})
-		end,
-	},
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local config = require("nvim-treesitter.configs")
+      config.setup({
+        ensure_installed = {
+          "lua",
+          "javascript",
+          "cpp",
+          "css",
+          "html",
+          "nix",
+          "scss",
+          "typescript",
+          "yaml",
+          "json",
+          "toml",
+          "python",
+          "fish",
+          "bash",
+          "c",
+          "dockerfile",
+          "tsx",
+          "gitignore",
+          "hcl",
+        },
+        highlight = true,
+        sync_install = false,
+        indent = true,
+      })
+    end,
+  },
 }
