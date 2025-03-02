248584702201c2c3bb9129e502c957671c5cd2ee
Weston Wood <stakin101@gmail.com>
2025-03-01 16:12:52 -0800

    added animate to setup

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -3,6 +3,7 @@ return {
 		"echasnovski/mini.nvim",
 		version = "0.15",
 		config = function()
+			require("mini.animate").setup()
 			require("mini.pairs").setup()
 			require("mini.surround").setup()
 			require("mini.comment").setup()
